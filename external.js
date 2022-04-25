7b5216d7e851ffcfc4c79c112538838343ade0be
saifiahmada <saifiahmada@gmail.com>
2022-04-25 16:10:23 +0800

    2022 04 25

---
+++ external.js
@@ -0,0 +1,8 @@
+
+function ubah() {
+	document.getElementById("demo").innerHTML = 'ubah paragrap';
+}
+
+function myFunction() {
+    document.getElementById("demo").innerHTML="Paragraph changed.";
+}
